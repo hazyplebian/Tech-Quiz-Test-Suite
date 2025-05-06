a2ddadefb5289aeddce2227609b8b60d063a6649
hazyplebian <michael.mangieri@yahoo.com>
2025-05-06 18:14:54 -0400

    final configuration of tests, got them passing!

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'cypress';
-import customViteConfig from './vite.config';
+import { defineConfig } from "cypress";
+import customViteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
-      viteConfig: customViteConfig,
+      framework: "react",
+      bundler: "vite",
     },
-    specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: false
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
